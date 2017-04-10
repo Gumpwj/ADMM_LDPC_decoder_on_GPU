56047bedbbfb9780cc2d7953297931e03dd49a20
Gumpwj <18801169722@163.com>
2017-04-10 21:19:18 +0800

    0410

---
+++ src/ldpc/ADMM_GPU_decoder_64b.h
@@ -0,0 +1,68 @@
+/*
+ *  ldcp_decoder.h
+ *  ldpc3
+ *
+ *  Created by legal on 02/04/11.
+ *  Copyright 2011 ENSEIRB. All rights reserved.
+ *
+ */
+
+/*----------------------------------------------------------------------------*/
+
+#include "../custom/custom_cuda_64b.h"
+
+#define NUMBER_OF_FRAMES     1
+
+#if 0
+	#define NOEUD       4000
+	#define PARITE      2000
+	#define MESSAGES    12000
+#else
+	#define NOEUD       2640
+	#define PARITE      1320
+	#define MESSAGES    7920
+#endif
+
+//#define nb_Node         NOEUD
+//#define nb_Check       PARITE
+//#define nb_Msg       MESSAGES
+
+//#define sizeNode  (NUMBER_OF_FRAMES * nb_Node)
+//#define sizeCheck (NUMBER_OF_FRAMES * nb_Check)
+//#define sizeMsgs  (NUMBER_OF_FRAMES * nb_Msg)
+
+
+class ADMM_GPU_decoder_64b{
+
+private:
+    double* h_iLLR;
+    double* d_iLLR;
+
+    double* d_oLLR;
+
+    int*   h_hDecision;
+    int*   d_hDecision;
+
+    unsigned int* d_t_row;
+    unsigned int* d_t_col;//1;
+
+    double* LZr;
+
+    unsigned int* d_degVNs;
+    unsigned int* d_degCNs;
+
+    unsigned int frames;
+    unsigned int VNs_per_frame;
+    unsigned int CNs_per_frame;
+    unsigned int MSGs_per_frame;
+    unsigned int VNs_per_load;
+    unsigned int CNs_per_load;
+    unsigned int MSGs_per_load;
+
+public:
+    ADMM_GPU_decoder_64b( int frames );
+    ~ADMM_GPU_decoder_64b();
+    void initialize();
+    void decode(double* llrs, int* bits, int nb_iters);
+};
+
